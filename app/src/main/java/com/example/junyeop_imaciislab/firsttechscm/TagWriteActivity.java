34535fe49984e9c7a7d607772e6d3828033af8fe
JunYeopLee <duql2072@gmail.com>
2015-11-05 04:41:45 +0900

    delete tag write test code

--- app/src/main/java/com/example/junyeop_imaciislab/firsttechscm/TagWriteActivity.java
+++ app/src/main/java/com/example/junyeop_imaciislab/firsttechscm/TagWriteActivity.java
@@ -48,8 +48,8 @@ public class TagWriteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tag_write);
         ButterKnife.inject(this);
         context = this;
-        //NFCtagID = getNFCtagID();
-        NFCtagID = "04b8d1496b0280"; // For Test
+        NFCtagID = getNFCtagID();
+        //NFCtagID = "04b8d1496b0280"; // For Test
         taggingTime = getIntent().getExtras().getLong("TaggingTime");
     }
 
